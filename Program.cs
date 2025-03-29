13ed530a6ce09591266f61f91c04b3f13e33f43f
s22818 <s22818@pjwstk.edu.pl>
2025-03-29 05:57:25 +0100

    Modyfikacja 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(){}
+
+public class Kontener 
+{
+    public int masaLadunku; //kilogramy
+    public int wysokosc; //centymetry
+    public int wagaWlasna; //sam kontener, kilogramy
+    public int glebokosc; //centymetry
+    public string numerSeryjny; //format: KON-C-1
+    public int maksymalnaLadownosc; //kilogramy
+
+    public Kontener()
+    {
+
+    }
+
+    void OproznienieLadunku()
+    {
+        Console.WriteLine("Ladunek o masie {0}, oprozniony", this.masaLadunku);
+
+        this.masaLadunku = 0;
+    }
+
+    void ZaladowanieLadunku(int masaLadunku)
+    {
+        try 
+        {
+            if(masaLadunku < this.maksymalnaLadownosc)
+            {
+                Console.WriteLine("Ladunek o masie " + masaLadunku + ", zaladowany");
+            }
+            else
+            {
+                throw new OverfillException("Ladunek o masie " + masaLadunku + ", jest za ciezki");
+            }        
+        }
+        catch (OverfillException e) 
+        {
+            Console.WriteLine(e);
+        } 
+    }
+}
+
+class OverfillException(string message) : Exception(message);
